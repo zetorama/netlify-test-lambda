a5049cbbecb07dcb789f0b8f3ce16ac46a2dd30e
Pavel Zet <zetorama@gmail.com>
2018-12-04 21:50:03 -0800

    fix(lambda): make `hello` work

--- lambda/hello.js
+++ lambda/hello.js
@@ -4,10 +4,22 @@ const obj = {
     requested: 0,
 }
 
+const delay = time => new Promise(ok => setTimeout(ok, time))
+
 exports.handler = async function(event, context, callback) {
+    console.log('1: start')
+
     callback(null, {
-        event,
-        context,
-        ...obj,
+
+        statusCode: 200,
+        body: JSON.stringify({
+            event,
+            context,
+            ...obj,
+        }),
     })
+
+    console.log('2: delay 1s')
+    await delay(1e3)
+    console.log('3: end')
 }
